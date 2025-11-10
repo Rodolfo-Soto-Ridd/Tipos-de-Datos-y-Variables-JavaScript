deb259f4cf2bb1420b80deb5253ca63e6612cfcd
rodasor <rodasor@gmail.com>
2025-11-10 20:09:20 -0300

    feat: primeros comandos de javascript

---
+++ datos_variables.js
@@ -0,0 +1,18 @@
+let nombre = "Juan";
+let apellido = 'Perez';
+let mensaje = `Hola ${nombre}`; // template literals
+
+let edad = 25;
+let precio = 99.99;
+let infinito = Infinity;
+let noEsNumero= NaN;
+
+let esActivo = true;
+let esMayor = false;
+
+let valorVacio = null;
+
+let simbolo = Symbol('descripcion');
+
+let numeroGrande = 123456789012345678901234567890n;
+
